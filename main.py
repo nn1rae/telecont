f8e482f88800d39d59701f6dee64a3d71114c838
nn1rae <orgithings@gmail.com>
2022-04-25 21:04:33 +0200

    new

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from hashlib import new
 import random
 import telebot
@@ -12,22 +11,30 @@ def new_user(id):
 quv = Query()
 
 def getdb(id,arg2 = 1):
-    return db.search(quv.userid == id)
-    # if arg2 == 0:
-    #     return new_get['userid']
-    # else :
-    #     return new_get['much']
+    get0 = db.search(quv.userid == id)
+    new_get = get0[0]
+    if arg2 == 0:
+        return new_get['userid']
+    else :
+        return new_get['prom']
 
 
 
 bot = telebot.TeleBot('5311428361:AAHmz1afEFRPBjN6fSHeARvarmyeNzsWIOA')
 
-print(str(getdb(999711677)))
 
 @bot.message_handler(commands=['start'])
 def start(messege):
-     user_id = messege.from_user.id
-     new_user(user_id)
+    user_id = messege.from_user.id
+    try:
+        getdb(user_id,0)
+        bot.send_message(messege.chat.id, f'Hello {messege.from_user.username}, wellcome back 😄, currently you have |{getdb(user_id)}| promo ')
+    except:
+        new_user(user_id)
+        bot.send_message(messege.chat.id, f'Hello {messege.from_user.username}, ?how you doing¿')
+
+
+     
 
      
     # bot.send_message(messege.chat.id, str(getdb(user_id)))
